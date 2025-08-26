00073c163e7efdf6fa9ce9a216ba32e41fb5b3bb
ducth <truonghuynhduc2k@gmail.com>
2025-08-26 17:13:50 +0700

    upgrade react native 0.77.1 => 0.80.2

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,14 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import {APP_SCREEN, RootStackParamList} from './screen-type';
-import {HomeScreen, ProfileScreen} from '../screens';
+import { APP_SCREEN, RootStackParamList } from './screen-type';
+import { HomeScreen, ProfileScreen } from '../screens';
+import { navigationRef } from './navigation-services';
 
 export const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Navigations: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator initialRouteName={APP_SCREEN.HOME}>
         <Stack.Screen name={APP_SCREEN.HOME} component={HomeScreen} />
         <Stack.Screen name={APP_SCREEN.PROFILE} component={ProfileScreen} />
